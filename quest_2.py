90247c0ac846c39eaccbce9b398c2361a40fb7fc
Pedro S <pedrosfig@hotmail.com>
2020-02-18 16:49:39 -0300

    todos os dias da semana

--- quest_2.py
+++ quest_2.py
@@ -1,9 +1,9 @@
 #Questão 2
 
-dias = {"1":"domingo","2":"Segunda","3":"Terça"}
+dias = {"1":"domingo","2":"Segunda","3":"Terça", "4":"Quarta","5":"Quinta","6":"Sexta","7":"Sábado"}
 
 num = input("escolha um dia: ")
-if int(num) <=3:
+if int(num) <=7:
     print(dias.get(num))
 else:
     print("dia invalido")
