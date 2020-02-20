95eac7b3714e52bbdd54e613c65eb0c077835ed2
PedroS <pedrosfig@hotmail.com>
2020-02-20 15:34:23 -0300

    questao 5 com função

--- quest_5.py
+++ quest_5.py
@@ -2,12 +2,19 @@
 #Questão 5
 import math as mt
 
-raio = input("Raio: ")
-if raio.isnumeric:
-    A = float(raio) * mt.pi**2
-    C = 2 * mt.pi * float(raio)
-    print("Círculo de raio ", float(raio), " terá área igual a ", round(A,2), " e perímetro igual a ", round(C,2))
-else:
-    "passar valor numérico"
+def circulo(raio):
+    texto = str(raio)
+    if texto.isnumeric():
+        A = float(texto) * mt.pi**2
+        C = 2 * mt.pi * float(texto)
+        resposta = "Círculo de raio " +texto + " terá área igual a " + str(round(A,2)) + " e perímetro igual a " + str(round(C,2))
+        return resposta
+    else:
+        return "passar valor numérico"
+
+x = "cinco"
+print(circulo(x))
 
 
+y = 10
+print(circulo(y))
