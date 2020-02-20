a50fa6020887892bc01a4d438ac527c831d24f1e
PedroS <pedrosfig@hotmail.com>
2020-02-20 15:53:25 -0300

    melhorei q5

--- quest_5.py
+++ quest_5.py
@@ -5,8 +5,8 @@ import math as mt
 def circulo(raio):
     texto = str(raio)
     if texto.isnumeric():
-        A = float(texto) * mt.pi**2
-        C = 2 * mt.pi * float(texto)
+        A = raio * mt.pi**2
+        C = 2 * mt.pi * raio
         resposta = "Círculo de raio " +texto + " terá área igual a " + str(round(A,2)) + " e perímetro igual a " + str(round(C,2))
         return resposta
     else:
