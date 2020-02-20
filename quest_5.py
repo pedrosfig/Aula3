129a4cdc31155765b7c4b2c368d2f708b1a2e577
PedroS <pedrosfig@hotmail.com>
2020-02-20 16:02:14 -0300

    melhorei q5

--- quest_5.py
+++ quest_5.py
@@ -18,3 +18,26 @@ print(circulo(x))
 
 y = 10
 print(circulo(y))
+
+
+
+
+
+def circulo2(raio):
+    try:
+        float(raio)
+    except:
+        raio = 0
+        print("Valor não numerico passado.")
+    A = raio * mt.pi**2
+    C = 2 * mt.pi * raio
+    resposta = "Círculo de raio " +str(raio) + " terá área igual a " + str(round(A,2)) + " e perímetro igual a " + str(round(C,2))
+    return resposta
+
+
+x = "cinco"
+print(circulo2(x))
+
+
+y = 10
+print(circulo2(y))
